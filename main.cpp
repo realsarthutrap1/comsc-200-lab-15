bb43b8afff340337492fef83b7c4710d23818bf6
sarthucodesagain <261507123+realsarthutrap1@users.noreply.github.com>
2026-09-23 11:38:24 -0700

    lab15: add movie class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 // COMSC-200 | Lab 15
 #include <iostream>
+#include <string>
 using namespace std;
+
+class Movie {
+private:
+    // data for one movie
+    string title;
+    int yearReleased;
+    string screenWriter;
+
+public:
+    void setTitle(string value);
+    void setYearReleased(int value);
+    void setScreenWriter(string value);
+    string getTitle() const;
+    int getYearReleased() const;
+    string getScreenWriter() const;
+    void print() const;
+};
 
 int main() {
     return 0;
